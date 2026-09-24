4ff3be46d6b7ba1482077aa7de75ad73207737b2
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-09-24 17:02:26 +0100

    feat: implement index

--- resources/views/users/address/index.blade.php
+++ resources/views/users/address/index.blade.php
@@ -491,11 +491,10 @@
                                      </a>
                                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="seeMore" style="">
                                         
-                                         <!-- @if($transaction->addressVerificationDetail()->exists()) -->
-                                         <a class="dropdown-item" href="{{route('ViewCandidateAddresses', $transaction->hashid)}}">
+                                          <a class="dropdown-item" href="{{route('ViewCandidateAddresses', $transaction->hashid)}}">
                                              <i data-feather="eye" class="icon-xs me-1"></i> View Verifications
                                          </a>
-                                         <!-- @endif -->
+                                         
                                          <a class="dropdown-item" href="{{route('showVerificationDetailsForm', ['slug' => $slug->slug, 'service_ref' => $transaction->service_reference])}}">
                                              <i data-feather="plus-circle" class="icon-xs me-1"></i> Make a Verification Request
                                          </a>
